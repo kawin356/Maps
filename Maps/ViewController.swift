4db1d89cc0437947f99c0671f51ef5f08165d63d
Kittikawin <kawin356@gmail.com>
2020-05-09 14:39:42 +0700

    init

---
+++ Maps/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Kittikawin Sontinarakul on 9/5/2563 BE.
+//  Copyright © 2563 Kittikawin Sontinarakul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.0'
+
+target 'Maps' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Maps
+pod 'GoogleMaps'
+end
